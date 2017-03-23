311a4b829eb45bce381fe449ab1711ccdf11201e
Bryan S <bryan.sandras@hotmail.fr>
2017-03-23 14:55:13 +0100

    zonedessin en cours

---
+++ TP5/src/ZoneDessin.cpp
@@ -0,0 +1,12 @@
+#include "ZoneDessin.hpp"
+
+ZoneDessin::ZoneDessin()
+{
+  _window.set_title("First fenetre");
+  _window.set_default_size(640, 480);
+}
+
+ZoneDessin::~ZoneDessin()
+{
+  //return test
+}
